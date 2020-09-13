0733f10b91ba979d788776da304b4e038f079782
Cyrill Martin <cyrill.martin@gmail.com>
2020-09-13 17:45:08 +0200

    Add cookieconsent files locally

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,7 +14,6 @@ require("file-saver")
 require("packs/kmap")
 require("packs/download")
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
